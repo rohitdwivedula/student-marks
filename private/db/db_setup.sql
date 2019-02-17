5b04baa2a4848fb32c2e3ee2130ec17d9f9b16fe
rohitdwivedula <rohitdwivedula@gmail.com>
2019-02-17 23:51:58 +0530

    Initial commit

---
+++ private/db/db_setup.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE student_marks;
+USE student_marks;
+
+CREATE TABLE users (
+	user_email varchar(255) NOT NULL,
+	user_key varchar(255) NOT NULL,
+	name varchar(255),
+	admin bit,
+	PRIMARY KEY (user_email)
+);
+
+CREATE TABLE eval (
+	eval_id int NOT NULL AUTO_INCREMENT,
+	eval_name varchar(255) NOT NULL,
+	date_of_eval date NOT NULL,
+	description varchar(255),
+	max_marks int NOT NULL,
+	PRIMARY KEY (eval_id)
+);
+
+CREATE TABLE marks (
+	record_number int NOT NULL AUTO_INCREMENT,
+	user_email varchar(255) NOT NULL,
+	eval_id int NOT NULL,
+	score int NOT NULL,
+	PRIMARY KEY (record_number),
+	FOREIGN KEY (user_email) REFERENCES users(user_email),
+	FOREIGN KEY (eval_id) REFERENCES eval (eval_id)
+);
+
+ALTER TABLE marks ADD UNIQUE(user_email, eval_id);
